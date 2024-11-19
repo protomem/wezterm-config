845c580ef84d2b4742e6e7faf081323b277c3466
protomem <protomem0709@gmail.com>
2024-11-19 14:51:46 +0300

    git init

---
+++ wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.enable_tab_bar = false
+
+config.window_close_confirmation = "NeverPrompt"
+config.window_decorations = "TITLE | RESIZE" -- for macOS
+-- config.window_background_opacity = 0.85
+-- config.macos_window_background_blur = 10
+
+config.initial_cols = 120
+config.initial_rows = 37
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.color_scheme = "flexoki-dark"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+config.font_size = 14
+
+return config
