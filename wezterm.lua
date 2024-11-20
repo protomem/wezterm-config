ec2b740480f533847fd986bbed87156f2d01f72c
protomem <protomem0709@gmail.com>
2024-11-20 11:21:46 +0300

    update config

--- wezterm.lua
+++ wezterm.lua
@@ -5,7 +5,7 @@ config.automatically_reload_config = true
 config.enable_tab_bar = false
 
 config.window_close_confirmation = "NeverPrompt"
-config.window_decorations = "TITLE | RESIZE" -- for macOS
+config.window_decorations = "TITLE|RESIZE"
 -- config.window_background_opacity = 0.85
 -- config.macos_window_background_blur = 10
 
@@ -13,10 +13,18 @@ config.initial_cols = 120
 config.initial_rows = 37
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.keys = {
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 config.color_scheme = "Tokyo Night"
